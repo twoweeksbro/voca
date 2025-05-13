84c8f75400bd16c9fbec3712ef643218e5538df0
twoweeksbro <twoweeksbro@gmail.com>
2025-05-13 22:48:37 +0900

    #6 complete

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -2,12 +2,23 @@ import World from "./World";
 import styles from'./Hello.module.css'
 
 const Hello = () => {
+
+    function showName() {
+        console.log('Mike');
+    }
+    function showText(e){
+        console.log(e.target.value)
+    }
     return(
         <div>
             <h1>Hello</h1>
-            <World />  
-            <World />  
-            <div className={styles.box} >Hello</div>
+            <button onClick={showName}>Show name</button>
+            <button 
+            onClick={() => {
+                    console.log(30);
+                }}
+            >Show age</button>
+        <input type='text' onChange={showText} />
         </div>
 
     ); 
