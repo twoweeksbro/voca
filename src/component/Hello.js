9e9e2cc516ca5575e7a2ab9c07a251dd452893da
twoweeksbro <twoweeksbro@gmail.com>
2025-05-13 23:01:23 +0900

    #7 complete:state

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,24 +1,22 @@
 import World from "./World";
 import styles from'./Hello.module.css'
+import { useState } from "react";
 
 const Hello = () => {
+    // let name = 'Mike'
+    const [name, setName] = useState('Mike');
 
-    function showName() {
-        console.log('Mike');
-    }
-    function showText(e){
-        console.log(e.target.value)
+    function changeName(){
+        const newName = name === 'Mike' ? 'Jane' : 'Mike';
+        console.log(name);
+        // document.getElementById('name').innerText = name;
+        setName(newName)
     }
     return(
         <div>
-            <h1>Hello</h1>
-            <button onClick={showName}>Show name</button>
-            <button 
-            onClick={() => {
-                    console.log(30);
-                }}
-            >Show age</button>
-        <input type='text' onChange={showText} />
+            <h1>state</h1>
+            <h2 id='name'>{name}</h2>
+            <button onClick={changeName}>Change</button>
         </div>
 
     ); 
