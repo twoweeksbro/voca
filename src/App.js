4f2ab2b4a2acca278f5cab3f1a439b96e69cdf22
twoweeksbro <twoweeksbro@gmail.com>
2025-05-14 23:30:01 +0900

    #9 complete:map

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import './App.css';
-import Hello from './component/Hello';
-import Welcome from './component/Welcome';
-import styles from './App.module.css';
+import Day from "./component/Day";
+import DayList from "./component/DayList";
+import Header from "./component/Header";
 
 function App() {
   return (
     <div className="App">
-      <Hello age={10}/>
-      <Hello age={20}/>
-      <Hello age={30}/>
-      <div className={styles.box}>App </div>
+      <Header />
+      <DayList />
+      <Day />
     </div>
   );
 }
